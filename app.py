a9ffa5f1969bb16423f48690194ba241ac9f18bc
vaishnavi-sidhu <cu24250062@coeruniversity.ac.in>
2026-06-12 12:43:02 +0530

    Create app.py file

---
+++ app.py
@@ -0,0 +1,12 @@
+subject = input("Enter Subject: ")
+exam_date = input("Enter Exam Date: ")
+
+print("\n===== AI STUDY PLAN =====")
+print(f"Subject: {subject}")
+print(f"Exam Date: {exam_date}")
+
+print("\nStudy Schedule:")
+print("Week 1: Learn basics and concepts")
+print("Week 2: Practice important topics")
+print("Week 3: Solve previous questions")
+print("Final Days: Revision and mock tests")
